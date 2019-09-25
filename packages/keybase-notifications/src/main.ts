7cf74229402d33884fdf8f6b07705de8e30ce764
Marvin Pinto <git@pinto.im>
2019-09-25 14:13:58 -0400

    build: run the generated action locally on specific events

--- packages/keybase-notifications/src/main.ts
+++ packages/keybase-notifications/src/main.ts
@@ -1,8 +1,12 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 import {wait} from './wait';
 
 async function run() {
   try {
+    const context = github.context;
+    console.log(`Context is: " ${JSON.stringify(context)}`);
+
     const ms = core.getInput('milliseconds');
     console.log(`Waiting ${ms} milliseconds ...`);
 
@@ -11,8 +15,10 @@ async function run() {
     core.debug(new Date().toTimeString());
 
     core.setOutput('time', new Date().toTimeString());
+    return;
   } catch (error) {
     core.setFailed(error.message);
+    return;
   }
 }
 
