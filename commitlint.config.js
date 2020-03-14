b6dc6de496d4817c5ec0cb259691d0a62680eb13
Marvin Pinto <git@pinto.im>
2020-03-14 08:25:03 -0400

    build: update the config to allow dependabot PRs

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes'],
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [2, 'always', ['deps', 'keybase-notifications', 'automatic-releases']],
+  },
 };
