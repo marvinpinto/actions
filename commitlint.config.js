d398eea30d44a78e6d9c4ff2f793759702aa2cff
Marvin Pinto <git@pinto.im>
2021-03-23 14:54:02 -0400

    ci: add the "deps-dev" scope so dependabot commit messages pass

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ['deps', 'keybase-notifications', 'automatic-releases']],
+    'scope-enum': [2, 'always', ['deps', 'deps-dev', 'keybase-notifications', 'automatic-releases']],
     'footer-max-line-length': [0, 'never'],
     'body-max-line-length': [0, 'never'],
   },
