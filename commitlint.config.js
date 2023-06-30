328265036826bc873386bbca942c7eac14869310
Santiago Bernhardt <santiago.bernhardt@gmail.com>
2023-07-01 09:18:16 +1200

    Disable commit linting

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,4 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-enum': [
-      2,
-      'always',
-      ['deps', 'deps-dev', 'automatic-releases', 'keybase-notifications', 'automatic-releases'],
-    ],
-    'footer-max-line-length': [0, 'never'],
-    'body-max-line-length': [0, 'never'],
-  },
-  ignores: [
-    (message) =>
-      message.startsWith('chore(deps): ') ||
-      message.includes('Update Github Runner Node version and deprecate set-output'),
-  ],
+  rules: {},
 };
