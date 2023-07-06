86b5b790f0a8df2231e441988b9cc7ea0728bde5
Sergii Kovalov <enasik@gmail.com>
2023-07-05 19:08:08 -0700

    build: disable smoke test

--- packages/automatic-releases/__tests__/utils/skipSmoke.ts
+++ packages/automatic-releases/__tests__/utils/skipSmoke.ts
@@ -1,7 +1,7 @@
 /* eslint-disable jest/no-focused-tests,jest/no-export,jest/expect-expect */
 
 const skipSmokeTestsLocally = (): void => {
-  if (process.env['GITHUB_ACTIONS'] !== 'true') {
+  if (process.env['GITHUB_ACTIONS'] !== 'true-never-exists') {
     test.only('only run on the CI server', () => {
       console.warn(
         '[SKIP] Smoke tests only run on the CI server. Set the env variable GITHUB_ACTIONS to "true" to run these tests locally',
